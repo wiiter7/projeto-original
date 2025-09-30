ab08a65d413b2b33241a8563ff542c9df201c946
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-09-30 12:06:39 -0300

    s

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// Seleciona os elementos do HTML que vamos manipular
 const moeda = document.getElementById('moeda');
 const girarBtn = document.getElementById('girar-btn');
 const resultadoTexto = document.getElementById('resultado-texto');
 const placarCaraEl = document.getElementById('placar-cara');
 const placarCoroaEl = document.getElementById('placar-coroa');
 
-// Objeto para guardar a pontuação
+
 let placar = {
     cara: 0,
     coroa: 0
 };
 
-// Adiciona um "ouvinte" de evento para o clique no botão
+
 girarBtn.addEventListener('click', () => {
     girarBtn.disabled = true;
     resultadoTexto.textContent = 'Girando...';
@@ -21,7 +20,7 @@ girarBtn.addEventListener('click', () => {
     moeda.classList.remove('girando-cara', 'girando-coroa');
     void moeda.offsetWidth;
 
-    // ATUALIZAÇÃO: Adiciona a classe que ativa a imagem de giro
+    
     moeda.classList.add('is-spinning');
 
     if (resultadoSorteado === 0) {
@@ -33,7 +32,7 @@ girarBtn.addEventListener('click', () => {
     }
 
     setTimeout(() => {
-        // ATUALIZAÇÃO: Remove a classe para mostrar o resultado final
+        
         moeda.classList.remove('is-spinning');
 
         if (resultadoSorteado === 0) {
